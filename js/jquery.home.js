9390654f70f937e4aaba9bdf3d90880d0a167047
kelly <wangyanli0905@gmail.com>
2012-11-30 18:09:17 +0800

    Change
    Change

--- js/jquery.home.js
+++ js/jquery.home.js
@@ -1,3 +1,5 @@
+$.mobile.changePage( "index.html", { transition: "slideup"} );	
+
 // JavaScript Document
 
  $(document).ready(function () {
@@ -19,10 +21,13 @@
         });
 		
 		
-		
-	 /*图片旋转*/
-            $(".carousel1").bind("mousedown",
+	
+	 /*图片左滑*/
+           $(".carousel1").bind("swipeleft",
+	
                 function () {
+				 $.mobile.allowCrossDomainPages = true;
+					
                     startLeft = mouseMove().x - parseInt(getStyle($("#carousel1")[0], "left"));
                     isMove = true;
                 });
@@ -36,7 +41,7 @@
                     $("#carousel1").animate({ "left": maxleft + "px" }, 1024);
                 }
             });
-            $(".carousel1").bind("mousemove", function () {
+            $(".carousel1").bind("swipeleft", function () {
                 if (isMove) {
                     var pos = mouseMove();
                     var top = pos.y - startLeft;
@@ -121,79 +126,79 @@
 	
 	//触屏判断
 	var getDragClass=(function(){
-var SupportsTouches = ("createTouch" in document),//判断是否支持触摸
-StartEvent = SupportsTouches ? "touchstart" : "mousedown",//支持触摸式使用相应的事件替代
-MoveEvent = SupportsTouches ? "touchmove" : "mousemove",
-EndEvent = SupportsTouches ? "touchend" : "mouseup",
-$=function(id){
-return document.getElementById(id);
-},
-preventDefault=function(ev){
-if(ev)ev.preventDefault();
-else window.event.returnValue = false;
-},
-getMousePoint=function(ev){
-var x = y = 0,
-doc = document.documentElement,
-body = document.body;
-if(!ev) ev=window.event;
-if (window.pageYoffset) {
-x = window.pageXOffset;
-y = window.pageYOffset;
-}else{
-x = (doc && doc.scrollLeft || body && body.scrollLeft || 0) - (doc && doc.clientLeft || body && body.clientLeft || 0);
-y = (doc && doc.scrollTop  || body && body.scrollTop  || 0) - (doc && doc.clientTop  || body && body.clientTop  || 0);
-}
-if(SupportsTouches){
-var evt = ev.touches.item(0);//仅支持单点触摸,第一个触摸点
-x=evt.pageX;
-y=evt.pageY;
-}else{
-x += ev.clientX;
-y += ev.clientY;
-}
-return {'x' : x, 'y' : y};
-};
-function _drag(opt){
-this.el=typeof opt.el=='string'?$(opt.el):opt.el;//被拖动节点
-this.onstart=opt.start || new Function();//
-        this.onmove=opt.move || new Function();
-this.onend=opt.end || new Function();
-this.action=false;
-this.init();
-}
-_drag.prototype={
-init:function(){
-this.el.style.position='relative';
-this.el['on'+StartEvent]=this.bind(function(e){//绑定节点的 [鼠标按下/触摸开始] 事件
-                preventDefault(e);
-if(this.action)return false;
-else this.action=true;
-this.startPoint=getMousePoint(e);
-this.onstart();
-document['on'+MoveEvent]=this.bind(function(e){
-preventDefault(e);//取消文档的默认行为[鼠标移动、触摸移动]
-this.nowPoint=getMousePoint(e);
-this.el.style.left=this.nowPoint.x-this.startPoint.x+'px';
-this.el.style.top=this.nowPoint.y-this.startPoint.y+'px';
-this.onmove();
-},this);
-document['on'+EndEvent]=document['ontouchcancel']=this.bind(function(){
-document['on'+EndEvent]=document['ontouchcancel']=document['on'+MoveEvent]=null;
-this.action=false;
-this.onend();
-},this);
-},this);
-},
-bind:function(fn,obj){
-return function(){
-fn.apply(obj,arguments);
-}
-}
-}
-return function(opt){
-return new _drag(opt);
-}
-})();
-
-
+	var SupportsTouches = ("createTouch" in document),//判断是否支持触摸
+	StartEvent = SupportsTouches ? "touchstart" : "mousedown",//支持触摸式使用相应的事件替代
+	MoveEvent = SupportsTouches ? "touchmove" : "mousemove",
+	EndEvent = SupportsTouches ? "touchend" : "mouseup",
+	$=function(id){
+	return document.getElementById(id);
+	},
+	preventDefault=function(ev){
+	if(ev)ev.preventDefault();
+	else window.event.returnValue = false;
+	},
+	getMousePoint=function(ev){
+	var x = y = 0,
+	doc = document.documentElement,
+	body = document.body;
+	if(!ev) ev=window.event;
+	if (window.pageYoffset) {
+	x = window.pageXOffset;
+	y = window.pageYOffset;
+	}else{
+	x = (doc && doc.scrollLeft || body && body.scrollLeft || 0) - (doc && doc.clientLeft || body && body.clientLeft || 0);
+	y = (doc && doc.scrollTop  || body && body.scrollTop  || 0) - (doc && doc.clientTop  || body && body.clientTop  || 0);
+	}
+	if(SupportsTouches){
+	var evt = ev.touches.item(0);//仅支持单点触摸,第一个触摸点
+	x=evt.pageX;
+	y=evt.pageY;
+	}else{
+	x += ev.clientX;
+	y += ev.clientY;
+	}
+	return {'x' : x, 'y' : y};
+	};
+	function _drag(opt){
+	this.el=typeof opt.el=='string'?$(opt.el):opt.el;//被拖动节点
+	this.onstart=opt.start || new Function();//
+			this.onmove=opt.move || new Function();
+	this.onend=opt.end || new Function();
+	this.action=false;
+	this.init();
+	}
+	_drag.prototype={
+	init:function(){
+	this.el.style.position='relative';
+	this.el['on'+StartEvent]=this.bind(function(e){//绑定节点的 [鼠标按下/触摸开始] 事件
+					preventDefault(e);
+	if(this.action)return false;
+	else this.action=true;
+	this.startPoint=getMousePoint(e);
+	this.onstart();
+	document['on'+MoveEvent]=this.bind(function(e){
+	preventDefault(e);//取消文档的默认行为[鼠标移动、触摸移动]
+	this.nowPoint=getMousePoint(e);
+	this.el.style.left=this.nowPoint.x-this.startPoint.x+'px';
+	this.el.style.top=this.nowPoint.y-this.startPoint.y+'px';
+	this.onmove();
+	},this);
+	document['on'+EndEvent]=document['ontouchcancel']=this.bind(function(){
+	document['on'+EndEvent]=document['ontouchcancel']=document['on'+MoveEvent]=null;
+	this.action=false;
+	this.onend();
+	},this);
+	},this);
+	},
+	bind:function(fn,obj){
+	return function(){
+	fn.apply(obj,arguments);
+	}
+	}
+	}
+	return function(opt){
+	return new _drag(opt);
+	}
+	})();
+	
+	
