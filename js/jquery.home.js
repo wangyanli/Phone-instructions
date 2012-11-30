d600b0170bc7335e690545dffa436c208e4b578b
kelly <wangyanli0905@gmail.com>
2012-11-30 14:22:29 +0800

    Phone instructions
    Phone instructions

---
+++ js/jquery.home.js
@@ -0,0 +1,188 @@
+// JavaScript Document
+
+ $(document).ready(function () {
+            $("#carousel1").CloudCarousel({
+                xPos: 380,
+                yPos: 110,
+                FPS: 30,
+                reflHeight: 86,
+                reflGap: 2,
+                yRadius: 40,
+                autoRotateDelay: 1200,
+                speed: 0.2,
+                mouseWheel: true,
+                bringToFront: true
+            });
+
+            $(".btnback").focus(function () { $(this).blur(); });
+            $(".txtsearch").focus(function () { $(this).addClass("txtsearch_hover"); }).blur(function () { if ($(this).val() == "") { $(this).removeClass("txtsearch_hover"); } });
+        });
+		
+		
+		
+	 /*图片旋转*/
+            $(".carousel1").bind("mousedown",
+                function () {
+                    startLeft = mouseMove().x - parseInt(getStyle($("#carousel1")[0], "left"));
+                    isMove = true;
+                });
+            $(document).bind("mouseup", function () {
+                isMove = false;
+                if (parseInt($("#carousel1")[0].style.left) > 0) {
+                    $("#carousel1").animate({ "left": "0px" }, 1024);
+                }
+
+                if (parseInt($("#carousel1")[0].style.left) < maxleft) {
+                    $("#carousel1").animate({ "left": maxleft + "px" }, 1024);
+                }
+            });
+            $(".carousel1").bind("mousemove", function () {
+                if (isMove) {
+                    var pos = mouseMove();
+                    var top = pos.y - startLeft;
+                    $("#carousel1")[0].style.left = left + "px";
+                }
+            });
+	
+	
+	//弹出搜索框
+	function shows(i,a){
+		
+		if (i.style.display == "none") {
+		   i.style.display = "";
+		}else{
+		   i.style.display = "none";
+		}
+	}	
+	
+		
+	//弹出header层		
+		var id=0  
+		function chk_broswer(){  
+        if (navigator.appName.indexOf("Microsoft")!=-1)
+		 {  
+        return(true);  
+         }  
+         else{  
+                 return(false);  
+         }  
+	} 
+	 
+	function c(css)  
+	{
+			ie5menu.className=css;
+	} 
+		 
+	function showmenuie5(idh)
+	{  
+	   event.cancelBubble = true  
+		id=idh  
+	   ie5menu.style.left=document.body.scrollLeft+event.clientX  
+	   ie5menu.style.top=document.body.scrollTop+event.clientY  
+	   ie5menu.style.visibility="visible"  
+	   
+	   return false
+	 }  
+	 
+	function hidemenuie5(){
+		ie5menu.style.visibility="hidden"
+	}  
+	function pop(win)
+	{
+		window.open(win,'','')
+	}  
+	function linkUI(urlstring){  
+		var str=urlstring+id  
+		pop(str)  
+	}  
+			 
+	if (document.all&&window.print){  
+	if(chk_broswer() == true){  
+		window.document.onclick = hidemenuie5;  
+	} else {  
+		window.onclick = hidemenuie5;  
+	} 
+	 
+	} 
+	
+	
+	
+	
+	//触屏判断
+	var getDragClass=(function(){
+var SupportsTouches = ("createTouch" in document),//判断是否支持触摸
+StartEvent = SupportsTouches ? "touchstart" : "mousedown",//支持触摸式使用相应的事件替代
+MoveEvent = SupportsTouches ? "touchmove" : "mousemove",
+EndEvent = SupportsTouches ? "touchend" : "mouseup",
+$=function(id){
+return document.getElementById(id);
+},
+preventDefault=function(ev){
+if(ev)ev.preventDefault();
+else window.event.returnValue = false;
+},
+getMousePoint=function(ev){
+var x = y = 0,
+doc = document.documentElement,
+body = document.body;
+if(!ev) ev=window.event;
+if (window.pageYoffset) {
+x = window.pageXOffset;
+y = window.pageYOffset;
+}else{
+x = (doc && doc.scrollLeft || body && body.scrollLeft || 0) - (doc && doc.clientLeft || body && body.clientLeft || 0);
+y = (doc && doc.scrollTop  || body && body.scrollTop  || 0) - (doc && doc.clientTop  || body && body.clientTop  || 0);
+}
+if(SupportsTouches){
+var evt = ev.touches.item(0);//仅支持单点触摸,第一个触摸点
+x=evt.pageX;
+y=evt.pageY;
+}else{
+x += ev.clientX;
+y += ev.clientY;
+}
+return {'x' : x, 'y' : y};
+};
+function _drag(opt){
+this.el=typeof opt.el=='string'?$(opt.el):opt.el;//被拖动节点
+this.onstart=opt.start || new Function();//
+        this.onmove=opt.move || new Function();
+this.onend=opt.end || new Function();
+this.action=false;
+this.init();
+}
+_drag.prototype={
+init:function(){
+this.el.style.position='relative';
+this.el['on'+StartEvent]=this.bind(function(e){//绑定节点的 [鼠标按下/触摸开始] 事件
+                preventDefault(e);
+if(this.action)return false;
+else this.action=true;
+this.startPoint=getMousePoint(e);
+this.onstart();
+document['on'+MoveEvent]=this.bind(function(e){
+preventDefault(e);//取消文档的默认行为[鼠标移动、触摸移动]
+this.nowPoint=getMousePoint(e);
+this.el.style.left=this.nowPoint.x-this.startPoint.x+'px';
+this.el.style.top=this.nowPoint.y-this.startPoint.y+'px';
+this.onmove();
+},this);
+document['on'+EndEvent]=document['ontouchcancel']=this.bind(function(){
+document['on'+EndEvent]=document['ontouchcancel']=document['on'+MoveEvent]=null;
+this.action=false;
+this.onend();
+},this);
+},this);
+},
+bind:function(fn,obj){
+return function(){
+fn.apply(obj,arguments);
+}
+}
+}
+return function(opt){
+return new _drag(opt);
+}
+})();
+
+
---
+++ js/jquery.menu.js
@@ -0,0 +1,109 @@
+ var startTop = 0;
+        var isMove = false;
+        var li;
+        $(document).ready(function () {
+            /*文本框切换*/
+            $(".btnback").focus(function () { $(this).blur(); });
+            $(".txtsearch").focus(function () { $(this).addClass("txtsearch_hover"); }).blur(function () { if ($(this).val() == "") { $(this).removeClass("txtsearch_hover"); } });
+
+            var maxTop = parseInt(getStyle($("#content_left")[0], "height")) - parseInt(getStyle($("#content_left ul li")[0], "height")) * $("#content_left ul li").size();
+
+            /*左菜单效果*/
+            $(".content_left ul").bind("mousedown",
+                function () {
+                    startTop = mouseMove().y - parseInt(getStyle($("#content_left ul")[0], "top"));
+                    isMove = true;
+                });
+            $(document).bind("mouseup", function () {
+                isMove = false;
+                if (parseInt($("#content_left ul")[0].style.top) > 0) {
+                    $("#content_left ul").animate({ "top": "0px" }, 400);
+                }
+
+                if (parseInt($("#content_left ul")[0].style.top) < maxTop) {
+                    $("#content_left ul").animate({ "top": maxTop + "px" }, 400);
+                }
+            });
+            $(".content_left ul").bind("mousemove", function () {
+                if (isMove) {
+                    var pos = mouseMove();
+                    var top = pos.y - startTop;
+                    $("#content_left ul")[0].style.top = top + "px";
+                }
+            });
+
+            // Level 1 Menu click
+            $(".content_left ul li").bind("click", function () {
+                var _index = $(this).index();
+                if (li != this) {
+                    $("#menu_2").animate({ "margin-left": "-180px" }, 0, function () { $(this).animate({ "margin-left": "80px" }, 400) });
+                }
+                $("#menu_2>ul>li").hide().eq(_index).show();
+
+                if (_index == 5) {
+                    $(".explain").html("4英寸Super Clear LCD (可丽屏)，极致纤薄清晰绚丽。Android™ 2.2智能操作系统，尽享万千热门应用。1GHz 高速处理器，畅快体验淋漓尽致。");
+                }
+                else
+                { $(".explain").html(""); }
+                $("#video").html("");
+
+                $(this).parent().find("span").removeClass("span_color");  //字体颜色
+                $(this).parent().find("div").removeClass("display_block"); //箭头
+                $(this).find("span:eq(0)").addClass("span_color");      //字体颜色
+                $(this).find("div:eq(0)").addClass("display_block");    //箭头
+                li = this;
+            });
+
+            // Level 2 Menu click
+            $("#menu_2>ul>li li").bind("click", function () {
+                var _index = $("#menu_2>ul>li li").index(this);
+                //$(this).parent().parent().hide();
+                $("#menu_2").animate({ "margin-left": "-320px" }, 400);
+                $("#menu_3>ul>li").hide().eq(_index).show();
+                li = this;
+            });
+
+            // Level 3 Menu click
+            var vedios = ["naozong.mp4", "duanxin.mp4", "feixingmoshi.mp4", "qiangzhi.mp4", "shijianriqi.mp4", "WIFI.mp4", "yuyan.mp4"];
+            var explains = ["设置、连接并查找局域网络", "设置、连接并查找局域网络", "打开和关闭飞行模式", "设置闹钟", "更改桌面背景（墙纸）", "设置、调整系统显示时间和日期", "选择和设置系统显示语言"];
+            $("#menu_3>ul>li li").bind("click", function () {
+                var _index = $("#menu_3>ul>li li").index(this);
+                $(".explain").html(explains[_index]);
+
+                $("#video").html("<video id='myVideo1' height='400' width='240' autoplay='autoplay'>" +
+                                    "<source src='video/" + vedios[_index] + "' type='video/mp4' />" +
+                                    "<p>Your browser does not support the video tag.</p>" +
+				                "</video>");
+            });
+        });
+
+        // 鼠标坐标
+        function mousePosition(ev) {
+            var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+            var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            return {
+                x: ev.clientX + scrollLeft - document.documentElement.clientLeft,
+                y: ev.clientY + scrollTop - document.documentElement.clientTop
+            };
+        }
+
+        // 鼠标事件
+        function mouseMove(ev) {
+            ev = ev || window.event;
+            var mousePos = mousePosition(ev);
+            return { x: mousePos.x, y: mousePos.y };
+        }
+        //获取CSS样式表属性
+        function getStyle(node, property) {
+            if (node.style[property]) {
+                return node.style[property];
+            }
+            else if (node.currentStyle) {
+                return node.currentStyle[property];
+            }
+            else if (document.defaultView && document.defaultView.getComputedStyle) {
+                var style = document.defaultView.getComputedStyle(node, null);
+                return style.getPropertyValue(property);
+            }
+            return null;
+        }
